f7e9143209a123d45439b9adcd729b02e9a0a137
Haritha Hasathcharu <42366642+hasathcharu@users.noreply.github.com>
2022-12-27 14:44:03 +0530

    dependencies

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+
 const app = express();
+const cors = require('cors');
 
 // const adminRoutes = require('./routes/admin');
 // const shopRoutes = require('./routes/shop');
