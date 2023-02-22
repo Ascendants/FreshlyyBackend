1a6954bab986594d427e10c2c4e7fa4b9a67ef33
Harini Kavindya <kavindya22sh@gmail.com>
2023-02-22 22:58:09 +0530

    Insert Product Back-end developed

--- controllers/farmer.js
+++ controllers/farmer.js
@@ -19,3 +19,36 @@ exports.getDashboard = async (req, res, next) => {
 	const products = Product.find({ farmer: req.user._id });
 	res.status(200).json({ message: 'Success', user: req.user });
 };
+
+exports.insertProduct = async (req, res, next) => {
+	const user = await User.findOne({ email: 'komuthu@freshlyy.com' });
+	console.log(req.body);
+	const { price, quantity, description,title, minQtyIncrement } = req.body;
+	const newProduct = new Product({
+		title:title,
+		status: 'Paused',
+		description: description,
+		price: price,
+		overallRating: 3,
+		minQtyIncrement: minQtyIncrement,
+		unit: 'KG',
+		farmer:user,
+		qtyAvailable: quantity,
+		imageUrls: [
+			'https://firebasestorage.googleapis.com/v0/b/freshlyyimagestore.appspot.com/o/ProductImages%2FJackfruit_hanging.jpg?alt=media&token=3fab3b13-82b9-4ba8-b76f-3ea84bc1ceec',
+		],
+	});
+	newProduct.publicUrl = (
+		newProduct.title.replace(/ /g, '_') +
+		'_' +
+		ObjectId(newProduct)
+	).toLowerCase();
+	newProduct.save((err, savedProduct) => {
+		if (err) {
+		  console.error(err);
+		  res.status(500).send('Error saving product');
+		} else {
+		  res.status(200).json(savedProduct);
+		}
+	  });
+};
--- routes/farmer.js
+++ routes/farmer.js
@@ -8,5 +8,6 @@ const router = express.Router();
 // router.get('/user', publicController.createUser);
 router.get('/hello', farmerController.getHello);
 router.get('/dashboard', farmerController.getDashboard);
+router.post('/insertProduct',farmerController.insertProduct)
 
 module.exports = router;
