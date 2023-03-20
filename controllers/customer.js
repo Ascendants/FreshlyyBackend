0acab8f763c54e87d5bd57b0580423dbebd5c417
Nadun Rupasinghe <nadunmalindarupasinghe@gmail.com>
2023-03-20 10:53:24 +0530

    "Fixed the res"

--- controllers/customer.js
+++ controllers/customer.js
@@ -1077,10 +1077,10 @@ exports.getProducts = async (req, res, next) => {
     );
     const sortedResult = cheaperProducts.concat(expensiveProducts);
 
-    res.json(sortedResult);
+    res.status(200).json({message:'Success',mainPageProducts:sortedResult});
   } catch (error) {
     console.error(error);
-    res.status(500).send('Server Error! something is wrong');
+    res.status(500).json({message:'Unsuccessful'});
   }
 };
 
