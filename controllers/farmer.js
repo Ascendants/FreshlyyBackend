4d671c753fbd154daebc7e4da3fe9410d1a9e0d3
Komuthu <komuthuapsara@gmail.com>
2023-03-28 18:46:07 +0530

    Farmer dashboard WIP

--- controllers/farmer.js
+++ controllers/farmer.js
@@ -8,14 +8,29 @@ exports.getHello = async (req, res, next) => {
 	res.status(200).json({ message: 'Hello' });
 };
 
+
+//Geting all the farmer's dashboard data
 exports.getDashboard = async (req, res, next) => {
-	// const data = {
-	// 	fname: 'Nadun',
-	// 	lname: 'Fernando',
-	// 	imageUrl:
-	// 		'https://firebasestorage.googleapis.com/v0/b/freshlyyimagestore.appspot.com/o/UserImages%2Fkom.jpg?alt=media&token=49a88f0c-ab79-4d84-8ddb-ada16a2b0101',
-	// };
-	const orders = await Order.find({ farmer: req.user._id }); //gives all orders belonging to farmer;
-	const products = Product.find({ farmer: req.user._id });
-	res.status(200).json({ message: 'Success', user: req.user });
+  
+	const liveProducts = await Product.countDocuments({ farmer: req.user._id, status: 'Live' });
+	const pendingProducts = await Product.countDocuments({ farmer: req.user._id, status: 'Quarantined' });
+	
+	const pastOrders = await Order.countDocuments({ farmer: req.user._id, 'orderUpdate.closed': {$ne : null} }); 
+	const newOrders = await Order.countDocuments({
+		farmer: req.user._id,
+		$or: [
+			{'orderUpdate.processed': {$ne : null}},
+			{'orderUpdate.shipped': {$ne : null}},
+			{'orderUpdate.delivered': {$ne : null}},
+			{'orderUpdate.pickedUp': {$ne : null}},
+			{'orderUpdate.cancelled': {$ne : null}},
+			{'orderUpdate.failed': {$ne : null}},
+			{'orderUpdate.closed': {$ne : null}},
+		],
+		'orderUpdate.placed': {$ne : null},
+		'orderUpdate.payment': {$ne : null},
+	});
+
+	// console.log(newOrders);
+  res.status(200).json({ message: 'Success', user: req.user, liveProducts, pendingProducts, pastOrders, newOrders});
 };
